069c90ebb76bc86fb268c2942958a0d5350ae2a0
Brandon Kvarda <bjkvarda@gmail.com>
2022-05-05 14:56:00 -0700

    Updated username

--- notebook_module.py
+++ notebook_module.py
@@ -2,7 +2,7 @@
 # This file is an example of a job that needs to be executed on commit
 import sys
 # Include dependencies from this or other repo that is synced in Databricks repos
-sys.path.append("/Workspace/Repos/brandon.kvarda@databricks.com/repotests")
+sys.path.append("/Workspace/Repos/brandon@databricks.com/repotests")
 # Load the autoreload extension in notebooks where you want the dependencies to automatically be updated 
 %load_ext autoreload
 %autoreload 
